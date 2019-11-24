c739eacd3abbd1a8f9ac05d73940f78666e6deff
MrceL97 <9719marcel@gmail.com>
2019-11-24 18:44:54 +0100

    Added master code for wireless part

--- FinalProjectXbee/master/master.ino
+++ FinalProjectXbee/master/master.ino
@@ -1,18 +1,15 @@
 #include "Wire.h"
 #include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
+#include "SoftwareSerial.h"
 
 #define SLAVE_ADDR 0x04
 
-SoftwareSerial serial(2, 3);
+uint8_t value1 = 4, value2 = 2;
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+SoftwareSerial xbee(2,3);
+bool tables_on = false;
 
 typedef struct {
-  float acc_x;
-  float acc_y;
-  float acc_z;
-
   float humidity;
   float temperature;
 
@@ -23,14 +20,38 @@ sensor_data_t data;
 
 void send_fn() {
   // Raspberry requesting info
-  Wire.write((byte*)&data, sizeof(sensor_data_t));
+
+  if (tables_on) {
+    Serial.print("=");
+    Serial.println((uint8_t)(value1*value2));
+    Wire.write((uint8_t)(value1*value2));
+  } else {
+    Serial.println("Sending data...");
+    Serial.print("Struct contains:\n");
+    Serial.print("HUM: ");
+    Serial.println(data.humidity);
+    Serial.print("TMP: ");
+    Serial.println(data.temperature);
+    Serial.print("DST: ");
+    Serial.println(data.distance);
+    Serial.print("");
+    Wire.write((byte*)&data, sizeof(sensor_data_t));
+    /*
+     * Byte-to-Byte
+    uint8_t* d = (uint8_t*)&data;
+    int s = sizeof(d);
+    for (int i = 0; i < s; i++)
+      Wire.write(*d++); 
+    */
+  }
 }
 
 void setup()
 {
   Serial.begin(9600);
+  xbee.begin(9600);
   Wire.begin(SLAVE_ADDR);
-  initialize_accelerometer();
+  Wire.onRequest(send_fn);
 }
 
 void loop()
@@ -38,32 +59,20 @@ void loop()
   sensors_event_t event;
 
   // Request data to slave
-  while(!Serial.available());
-  
-  accel.getEvent(&event);
+  while(!xbee.available());
 
-  String message = Serial.readString();
-        
-  data.acc_x = event.acceleration.x;
-  data.acc_y = event.acceleration.y;
-  data.acc_z = event.acceleration.z;
+  String message = xbee.readString();
 
-  data.humidity = getValue(message, ',', 0).toFloat();
-  data.temperature = getValue(message, ',', 1).toFloat();
-  data.distance = getValue(message, ',', 2).toFloat();
-}
-
-void initialize_accelerometer()
-{
-  if (!accel.begin())
-  {
-    /* ADXL345 not detected... */
-    Serial.println("No ADXL345 detected...");
-    while (1)
-      ;
+  if (tables_on) {
+    Serial.print(value1);
+    Serial.print("x");
+    Serial.print(value2);
+  } else {
+    data.humidity = getValue(message, ',', 0).toFloat();
+    data.temperature = getValue(message, ',', 1).toFloat();
+    data.distance = getValue(message, ',', 2).toFloat();
   }
-
-  accel.setRange(ADXL345_RANGE_16_G);
+  Serial.println("Loop\n");
 }
 
 String getValue(String data, char separator, int index)
